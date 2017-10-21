d3b493e5a0c222deb927808fe03b1e3ceea5066a
jorge2244@gmail.com <jorge2244@gmail.com>
2017-10-21 12:24:15 -0200

    + Bug correction

--- api/controllers/palindromeController.js
+++ api/controllers/palindromeController.js
@@ -1,6 +1,6 @@
 exports.isAPalindrome = function(req, res) {
   var word = req.body.word;
-
+  console.log(word);
   var isPalindrome = palindromeAnalisis(word);
   if(isPalindrome){
     var msgStatus200 = {message: '200 : is a palindrome = '+ word};
@@ -17,28 +17,34 @@ function palindromeAnalisis(word){
   var odd = wordLength % 2;
 
   if(odd > 0){
-    verifyOddWord(word, wordLength);
+    verifyOddWord(word);
   }else {
-    return verifyPairWord(word, wordLength);
+    return verifyPairWord(word);
   }
 
   return true;
 }
 
-function verifyPairWord(word, length){
-  var halfLength = wordLength /2;
+function verifyPairWord(word){
+  var wordLength = word.length;
+  var halfLength = wordLength / 2;
   var firstHalf = word.substring(0, halfLength);
-  var secondHalf = word.substring(halfLength+1, wordLength);
+  var secondHalf = word.substring(halfLength, wordLength);
   var listFirstHalf = firstHalf.split("");
   var listSecondHalf = secondHalf.split("");
   var lastPosition = secondHalf.length;
-
+  var isPalindrome = true;
   listFirstHalf.forEach(function(letter){
+    //console.log(letter);
+    lastPosition = lastPosition - 1;
+    console.log(lastPosition);
+    console.log(letter + " = " + listSecondHalf[lastPosition]);
     if(letter != listSecondHalf[lastPosition]){
-      return false;
+      isPalindrome = false;
     }
-    lastPosition--;
   });
+
+  return isPalindrome;
 }
 
 function verifyOddWord(word, length){
